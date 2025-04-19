e1b43b475254aaf39dd416b1f4452ea3734f46f7
LC <changliu11@g.ucla.edu>
2025-04-19 00:06:28 -0700

    add file
    add simple training tasks for debug

--- examples/qwen2_5_vl_0_5b_math_grpo.sh
+++ examples/qwen2_5_vl_0_5b_math_grpo.sh
@@ -3,7 +3,7 @@ set -x
 MODEL_PATH=Qwen/Qwen2.5-0.5B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=examples/config.yaml \
+    config=examples/config_simple.yaml \
     data.train_files=hiyouga/math12k@train \
     data.val_files=hiyouga/math12k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
