eb84e574a6629f428d9845a38c8fab84e5d62186
LC <changliu11@g.ucla.edu>
2025-04-24 01:31:47 -0600

    Update qwen2_5_0_5b_math_grpo.sh

--- examples/qwen2_5_0_5b_math_grpo.sh
+++ examples/qwen2_5_0_5b_math_grpo.sh
@@ -8,4 +8,4 @@ python3 -m verl.trainer.main \
     data.val_files=hiyouga/math12k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen2_5_7b_math_grpo \
-    trainer.n_gpus_per_node=8
+    trainer.n_gpus_per_node=2
