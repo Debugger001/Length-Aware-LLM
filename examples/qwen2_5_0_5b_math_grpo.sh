04f02b6d7ff76c4c40dfb27dcff3d367bab244de
LC <changliu11@g.ucla.edu>
2025-05-01 15:03:30 -0700

    Update qwen2_5_0_5b_math_grpo.sh

--- examples/qwen2_5_0_5b_math_grpo.sh
+++ examples/qwen2_5_0_5b_math_grpo.sh
@@ -1,5 +1,7 @@
 set -x
 
+export PYTHONUNBUFFERED=1
+
 MODEL_PATH=Qwen/Qwen2.5-0.5B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
@@ -9,3 +11,5 @@ python3 -m verl.trainer.main \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=test_qwen2_5_0_5b_math_grpo \
     trainer.n_gpus_per_node=2
+    config=examples/config.yaml \
+    worker.actor.model.model_path=${MODEL_PATH}
