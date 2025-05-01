84ed3438a6728fc2b3a8b4f5c9903e889e07a6b9
LC <changliu11@g.ucla.edu>
2025-05-01 15:07:08 -0700

    Update qwen2_5_0_5b_math_grpo.sh

--- examples/qwen2_5_0_5b_math_grpo.sh
+++ examples/qwen2_5_0_5b_math_grpo.sh
@@ -7,7 +7,7 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=Qwen/Qwen2.5-0.5B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=examples/config_simple.yaml \
+    config=examples/config.yaml \
     data.train_files=hiyouga/math12k@train \
     data.val_files=hiyouga/math12k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
