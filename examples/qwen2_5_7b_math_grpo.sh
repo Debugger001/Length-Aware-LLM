5ce4a1b692455695b1b76b734e6b69975b98264e
LC <changliu11@g.ucla.edu>
2025-05-04 22:41:28 -0700

    edit

--- examples/qwen2_5_7b_math_grpo.sh
+++ examples/qwen2_5_7b_math_grpo.sh
@@ -8,4 +8,8 @@ MODEL_PATH=Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
-    worker.actor.model.model_path=${MODEL_PATH}
+    data.train_files=hiyouga/math12k@train \
+    data.val_files=hiyouga/math12k@test \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    trainer.experiment_name=test_qwen2_5_7b_math_grpo \
+    trainer.n_gpus_per_node=4
