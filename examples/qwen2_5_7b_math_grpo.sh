9af50142daa6f7fdae8ea3eafc9e964c160af953
LC <changliu11@g.ucla.edu>
2025-05-07 02:21:41 -0700

    Update qwen2_5_7b_math_grpo.sh

--- examples/qwen2_5_7b_math_grpo.sh
+++ examples/qwen2_5_7b_math_grpo.sh
@@ -12,4 +12,10 @@ python3 -m verl.trainer.main \
     data.val_files=hiyouga/math12k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=test_qwen2_5_7b_math_grpo \
-    trainer.n_gpus_per_node=4
+    trainer.n_gpus_per_node=4 \
+    algorithm.hit_cap_coef=50 \
+    algorithm.penalty_cap=0.1 \
+    algorithm.lambda_len_init=0.003 \
+    algorithm.dual_lr=0.004 \
+    algorithm.hit_cap_coef=100 \
+    algorithm.threshold=380
