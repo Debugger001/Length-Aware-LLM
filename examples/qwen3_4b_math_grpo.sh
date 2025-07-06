e9504e20e5b7fdbefa16c8327f04c0dd90443eee
LC <changliu11@g.ucla.edu>
2025-07-05 20:13:47 -0700

    Update qwen3_4b_math_grpo.sh

--- examples/qwen3_4b_math_grpo.sh
+++ examples/qwen3_4b_math_grpo.sh
@@ -5,9 +5,11 @@ set -x
 export PYTHONUNBUFFERED=1
 
 MODEL_PATH=Qwen/Qwen3-4B  # replace it with your local file path
+PROJECT_NAME=Length-LLM
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
     data.max_response_length=4096 \
     worker.actor.model.model_path=${MODEL_PATH} \
-    trainer.experiment_name=qwen3_4b_math_grpo
+    trainer.experiment_name=qwen3_4b_math_grpo \
+    trainer.project_name=${PROJECT_NAME}
