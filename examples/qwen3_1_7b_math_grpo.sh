53c2361e5f99c2dbb3b846a3b4d090f56c5fb3a6
LC <changliu11@g.ucla.edu>
2025-07-05 23:29:10 -0700

    Update qwen3_1_7b_math_grpo.sh

--- examples/qwen3_1_7b_math_grpo.sh
+++ examples/qwen3_1_7b_math_grpo.sh
@@ -7,7 +7,7 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=Qwen/Qwen3-1.7B  # replace it with your local file path
 PROJECT_NAME=Length-LLM
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 ray stop --force
 ray start --head
 
@@ -17,6 +17,7 @@ python3 -m verl.trainer.main \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen3_1.7b_math_grpo \
     trainer.project_name=${PROJECT_NAME} \
-    worker.actor.global_batch_size=32 \
+    worker.actor.global_batch_size=64 \
+    trainer.n_gpus_per_node=4 \
     trainer.save_freq=40 \
     data.rollout_batch_size=128
