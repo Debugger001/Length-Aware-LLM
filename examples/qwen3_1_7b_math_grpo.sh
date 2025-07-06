f42fbf51e6ab99d9b6eef78b9349bbf55957b70c
LC <changliu11@g.ucla.edu>
2025-07-05 23:26:44 -0700

    Update qwen3_1_7b_math_grpo.sh

--- examples/qwen3_1_7b_math_grpo.sh
+++ examples/qwen3_1_7b_math_grpo.sh
@@ -7,7 +7,7 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=Qwen/Qwen3-1.7B  # replace it with your local file path
 PROJECT_NAME=Length-LLM
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4
 ray stop --force
 ray start --head
 
