22f0d4157794cbd3fb163255bfab48695a56867f
LC <changliu11@g.ucla.edu>
2025-07-05 21:51:36 -0700

    Update qwen3_4b_math_grpo.sh

--- examples/qwen3_4b_math_grpo.sh
+++ examples/qwen3_4b_math_grpo.sh
@@ -4,6 +4,8 @@ set -x
 
 export PYTHONUNBUFFERED=1
 
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4
+
 MODEL_PATH=Qwen/Qwen3-4B  # replace it with your local file path
 PROJECT_NAME=Length-LLM
 
@@ -12,4 +14,7 @@ python3 -m verl.trainer.main \
     data.max_response_length=4096 \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen3_4b_math_grpo \
-    trainer.project_name=${PROJECT_NAME}
+    trainer.project_name=${PROJECT_NAME} \
+    worker.actor.global_batch_size=32 \
+    trainer.save_freq=40 \
+    data.rollout_batch_size=128
