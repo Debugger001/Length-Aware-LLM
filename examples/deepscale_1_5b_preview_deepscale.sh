7b645e97b2d0ea848b5b2fed97ff8c978e03f5f6
LC <changliu11@g.ucla.edu>
2025-07-18 15:06:55 -0700

    edit

--- examples/deepscale_1_5b_preview_deepscale.sh
+++ examples/deepscale_1_5b_preview_deepscale.sh
@@ -6,7 +6,7 @@ export PYTHONUNBUFFERED=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 MODEL_PATH=agentica-org/DeepScaleR-1.5B-Preview  # replace it with your local file path
-CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_deepscaler/global_step_25/
+# CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_deepscaler/global_step_25/
 PROJECT_NAME=Length-LLM
 
 ray stop --force
@@ -17,19 +17,18 @@ python3 -m verl.trainer.main \
     data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
     data.val_files=HuggingFaceH4/MATH-500@test \
     worker.actor.model.model_path=${MODEL_PATH} \
-    trainer.load_checkpoint_path=${CKPT_PATH} \
     data.rollout_batch_size=256 \
     data.max_response_length=2048 \
     worker.actor.global_batch_size=32 \
-    trainer.experiment_name=deepscale_1.5b_deepscaler \
+    trainer.experiment_name=deepscale_1.5b_deepscaler_800 \
     trainer.project_name=Length-LLM \
     trainer.n_gpus_per_node=8 \
     trainer.save_freq=20 \
     algorithm.penalty_cap=0.4 \
-    algorithm.lambda_len_init=0.011667 \
+    algorithm.lambda_len_init=0 \
     algorithm.dual_lr=0.0002 \
     algorithm.lambda_floor=0.0001 \
     algorithm.hit_cap=0.1 \
-    algorithm.threshold=500 \
+    algorithm.threshold=800 \
     data.seed=20250521 \
     worker.rollout.n=4
--- examples/deepscale_1_5b_preview_math.sh
+++ examples/deepscale_1_5b_preview_math.sh
@@ -3,10 +3,10 @@
 set -x
 
 export PYTHONUNBUFFERED=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 MODEL_PATH=agentica-org/DeepScaleR-1.5B-Preview  # replace it with your local file path
-CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_math/global_step_160/
+# CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_math/global_step_160/
 PROJECT_NAME=Length-LLM
 
 ray stop --force
@@ -22,14 +22,13 @@ python3 -m verl.trainer.main \
     worker.actor.global_batch_size=32 \
     trainer.experiment_name=deepscale_1.5b_deepscaler \
     trainer.project_name=Length-LLM \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=8 \
     trainer.save_freq=29 \
-    trainer.load_checkpoint_path=${CKPT_PATH} \
     algorithm.penalty_cap=0.4 \
     algorithm.lambda_len_init=0.0003 \
     algorithm.dual_lr=0.0002 \
     algorithm.lambda_floor=0.0001 \
     algorithm.hit_cap=0.1 \
-    algorithm.threshold=350 \
+    algorithm.threshold=500 \
     data.seed=20250521 \
-    worker.rollout.n=3 
+    worker.rollout.n=4 
