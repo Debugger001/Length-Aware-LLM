990999f9e516b0c2041291cdd5314cc0486e54ec
LC <changliu11@g.ucla.edu>
2025-07-20 11:41:02 -0700

    Update deepscale_1_5b_preview_deepscale.sh

--- examples/deepscale_1_5b_preview_deepscale.sh
+++ examples/deepscale_1_5b_preview_deepscale.sh
@@ -6,7 +6,7 @@ export PYTHONUNBUFFERED=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 MODEL_PATH=agentica-org/DeepScaleR-1.5B-Preview  # replace it with your local file path
-# CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_deepscaler/global_step_25/
+CKPT_PATH=/data/cliu/checkpoints/Length-LLM/deepscale_1.5b_math_500/global_step_200/
 PROJECT_NAME=Length-LLM
 
 ray stop --force
@@ -17,10 +17,11 @@ python3 -m verl.trainer.main \
     data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
     data.val_files=HuggingFaceH4/MATH-500@test \
     worker.actor.model.model_path=${MODEL_PATH} \
+    trainer.load_checkpoint_path=${CKPT_PATH} \
     data.rollout_batch_size=128 \
     data.max_response_length=2048 \
     worker.actor.global_batch_size=32 \
-    trainer.experiment_name=deepscale_1.5b_deepscaler_1000_ceil \
+    trainer.experiment_name=deepscale_1.5b_deepscaler_1000_ceil_cont \
     trainer.project_name=Length-LLM \
     trainer.n_gpus_per_node=4 \
     trainer.save_freq=20 \
@@ -28,8 +29,8 @@ python3 -m verl.trainer.main \
     algorithm.lambda_len_init=0 \
     algorithm.dual_lr=0.0002 \
     algorithm.lambda_floor=0.00001 \
-    algorithm.lambda_ceil=0.002 \
+    algorithm.lambda_ceil=0.02 \
     algorithm.hit_cap=0.1 \
-    algorithm.threshold=800 \
+    algorithm.threshold=850 \
     data.seed=20250521 \
     worker.rollout.n=4
