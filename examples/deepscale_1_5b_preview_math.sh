ce2a2c2b05dcc7d723c73caebeddb4411bff80aa
LC <changliu11@g.ucla.edu>
2025-07-22 22:42:15 -0700

    Update deepscale_1_5b_preview_math.sh

--- examples/deepscale_1_5b_preview_math.sh
+++ examples/deepscale_1_5b_preview_math.sh
@@ -6,7 +6,7 @@ export PYTHONUNBUFFERED=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 MODEL_PATH=agentica-org/DeepScaleR-1.5B-Preview  # replace it with your local file path
-CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_deepscaler_1000_0722/global_step_220/
+CKPT_PATH=/home/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepscale_1.5b_math_800_0721/global_step_200/
 PROJECT_NAME=Length-LLM
 
 ray stop --force
@@ -21,10 +21,11 @@ python3 -m verl.trainer.main \
     data.rollout_batch_size=128 \
     data.max_response_length=4096 \
     worker.actor.global_batch_size=32 \
-    trainer.experiment_name=deepscale_1.5b_math_500_0722 \
+    trainer.experiment_name=deepscale_1.5b_math_500_0723 \
     trainer.project_name=Length-LLM \
     trainer.n_gpus_per_node=4 \
     trainer.save_freq=10 \
+    trainer.save_limit=5 \
     algorithm.penalty_cap=0.4 \
     algorithm.lambda_len_init=0 \
     algorithm.dual_lr=0.0002 \
