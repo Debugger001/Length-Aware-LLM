5bcc8596e51d8bcf037ced589fbef5992dfab9f2
LC <changliu11@g.ucla.edu>
2025-08-04 19:02:19 -0700

    Update qwen3_32b_deepscaler.sh

--- examples/qwen3_32b_deepscaler.sh
+++ examples/qwen3_32b_deepscaler.sh
@@ -21,7 +21,7 @@ python3 -m verl.trainer.main \
     worker.actor.global_batch_size=32 \
     trainer.experiment_name=deepscale_1.5b_deepscaler_1200_0804 \
     trainer.project_name=Length-LLM \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=8 \
     trainer.save_freq=25 \
     algorithm.penalty_cap=0.4 \
     algorithm.lambda_len_init=0 \
