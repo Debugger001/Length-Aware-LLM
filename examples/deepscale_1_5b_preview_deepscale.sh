dde5c28bf86942170ddf0996b0ddecd47fdf8e32
LC <changliu11@g.ucla.edu>
2025-08-04 23:16:32 -0700

    Update deepscale_1_5b_preview_deepscale.sh

--- examples/deepscale_1_5b_preview_deepscale.sh
+++ examples/deepscale_1_5b_preview_deepscale.sh
@@ -6,6 +6,7 @@ export PYTHONUNBUFFERED=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 MODEL_PATH=agentica-org/DeepScaleR-1.5B-Preview  # replace it with your local file path
+CKPT_PATH=/data/cliu/checkpoints/Length-LLM/deepscale_1.5b_deepscaler_grpo_base/global_step_100/
 PROJECT_NAME=Length-LLM
 
 ray stop --force
@@ -16,10 +17,11 @@ python3 -m verl.trainer.main \
     data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
     data.val_files=HuggingFaceH4/MATH-500@test \
     worker.actor.model.model_path=${MODEL_PATH} \
+    trainer.load_checkpoint_path=${CKPT_PATH} \
     data.rollout_batch_size=128 \
     data.max_response_length=4096 \
     worker.actor.global_batch_size=32 \
-    trainer.experiment_name=deepscale_1.5b_deepscaler_grpo_base \
+    trainer.experiment_name=deepscale_1.5b_deepscaler_grpo_base_0804 \
     trainer.project_name=Length-LLM \
     trainer.n_gpus_per_node=4 \
     trainer.save_freq=25 \
