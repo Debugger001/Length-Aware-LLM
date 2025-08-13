fd6560253b3305cf5348d6b6ad742e470da937a5
LC <changliu11@g.ucla.edu>
2025-08-13 15:14:00 -0700

    Update qwen2_5_1_5b_ds_grpo.sh

--- examples/qwen2_5_1_5b_ds_grpo.sh
+++ examples/qwen2_5_1_5b_ds_grpo.sh
@@ -14,7 +14,7 @@ ray start --head
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
     data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
-    data.val_files=HuggingFaceH4/MATH-500 \
+    data.val_files=HuggingFaceH4/MATH-500@test \
     worker.actor.model.model_path=${MODEL_PATH} \
     data.rollout_batch_size=128 \
     data.max_response_length=4096 \
