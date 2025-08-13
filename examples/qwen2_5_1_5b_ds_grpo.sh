6660cdf973f628ed531d7f5128352126543c0f20
LC <changliu11@g.ucla.edu>
2025-08-13 14:03:03 -0700

    edit

--- examples/qwen2_5_1_5b_ds_grpo.sh
+++ examples/qwen2_5_1_5b_ds_grpo.sh
@@ -14,12 +14,12 @@ ray start --head
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
     data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
-    data.val_files=hiyouga/math12k@test \
+    data.val_files=HuggingFaceH4/MATH-500 \
     worker.actor.model.model_path=${MODEL_PATH} \
     data.rollout_batch_size=128 \
     data.max_response_length=4096 \
     worker.actor.global_batch_size=32 \
-    trainer.experiment_name=test_qwen2_5_1.5b_ds_grpo_0808 \
+    trainer.experiment_name=test_qwen2_5_1.5b_ds_grpo_0813 \
     trainer.project_name=Length-LLM \
     trainer.n_gpus_per_node=4 \
     trainer.save_freq=25 \
--- verl/trainer/ray_trainer.py
+++ verl/trainer/ray_trainer.py
@@ -377,6 +377,7 @@ class RayPPOTrainer:
 
     def _validate(self) -> Dict[str, Any]:
         reward_tensor_lst = []
+        response_lengths = []
         # Lists to collect samples for the table
         sample_inputs, sample_outputs, sample_labels, sample_scores = [], [], [], []
         reward_metrics_lst = defaultdict(list)
@@ -397,6 +398,10 @@ class RayPPOTrainer:
             test_gen_batch, pad_size = pad_dataproto_to_divisor(test_gen_batch, self.actor_rollout_ref_wg.world_size)
             test_output_gen_batch = self.actor_rollout_ref_wg.generate_sequences(test_gen_batch)
             test_output_gen_batch = unpad_dataproto(test_output_gen_batch, pad_size=pad_size * repeat_times)
+
+            response_mask = test_output_gen_batch.batch["response_mask"]
+            actual_lengths = response_mask.sum(dim=1).tolist()  # list of ints
+            response_lengths.extend(actual_lengths)
 
             # repeat to align with repeated responses in rollout
             test_batch = test_batch.repeat(repeat_times=repeat_times, interleave=True)
@@ -425,7 +430,9 @@ class RayPPOTrainer:
         self.val_reward_score = torch.cat(reward_tensor_lst, dim=0).sum(-1).mean().item()
         val_reward_metrics = {f"val/{key}_reward": value for key, value in reduce_metrics(reward_metrics_lst).items()}
         print("Finish validation.")
-        return {"val/reward_score": self.val_reward_score, **val_reward_metrics}
+        # return {"val/reward_score": self.val_reward_score, **val_reward_metrics}
+        mean_response_length = sum(response_lengths) / len(response_lengths)
+        return {"val/reward_score": self.val_reward_score, **val_reward_metrics, "val/mean_response_length": mean_response_length}
 
     def _balance_batch(self, batch: DataProto, metrics: Dict[str, Any], logging_prefix: str = "global_seqlen") -> None:
         """Reorder the data on single controller such that each dp rank gets similar total tokens"""
