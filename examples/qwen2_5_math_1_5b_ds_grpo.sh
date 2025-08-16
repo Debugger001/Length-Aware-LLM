c20427ff84967558831b6cd0efc8fdfc4b867d56
LC <changliu11@g.ucla.edu>
2025-08-15 22:25:21 -0700

    Create qwen2_5_math_1_5b_ds_grpo.sh

---
+++ examples/qwen2_5_math_1_5b_ds_grpo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+export PYTHONUNBUFFERED=1
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+MODEL_PATH=Qwen/Qwen2.5-Math-1.5B-Instruct  # replace it with your local file path
+PROJECT_NAME=Length-LLM
+
+ray stop --force
+ray start --head
+
+python3 -m verl.trainer.main \
+    config=examples/config.yaml \
+    data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
+    data.val_files=HuggingFaceH4/MATH-500@test \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    data.rollout_batch_size=128 \
+    data.max_response_length=4096 \
+    worker.actor.global_batch_size=32 \
+    trainer.experiment_name=qwen2_5_math_1.5b_ds_grpo_0815 \
+    trainer.project_name=Length-LLM \
+    trainer.n_gpus_per_node=4 \
+    trainer.save_freq=25 \
+    trainer.save_limit=4 \
+    data.seed=20250521 \
+    worker.rollout.n=4
