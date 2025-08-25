73a7b28638d603bb0082c83ef6d56bd738478254
LC <changliu11@g.ucla.edu>
2025-08-24 23:03:44 -0700

    Create qwen3_8b_ds_grpo.sh

---
+++ examples/qwen3_8b_ds_grpo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+export PYTHONUNBUFFERED=1
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+MODEL_PATH=Qwen/Qwen3-8B  # replace it with your local file path
+PROJECT_NAME=Length-LLM
+
+ray stop --force
+ray start --head
+
+python3 -m verl.trainer.main \
+    config=examples/config.yaml \
+    data.train_files=agentica-org/DeepScaleR-Preview-Dataset \
+    data.val_files=HuggingFaceH4/MATH-500@test \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    data.rollout_batch_size=128 \
+    data.max_response_length=4096 \
+    worker.actor.global_batch_size=32 \
+    trainer.experiment_name=qwen3_8b_ds_grpo_0825 \
+    trainer.project_name=Length-LLM \
+    trainer.n_gpus_per_node=8 \
+    trainer.save_freq=25 \
+    trainer.save_limit=4 \
+    data.seed=20250521 \
+    worker.rollout.n=4
