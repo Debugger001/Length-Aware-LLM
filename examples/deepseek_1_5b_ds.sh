88c2bf53a15fc72df6c9a4bfc4c6f25badb847eb
LC <changliu11@g.ucla.edu>
2025-11-24 22:02:17 -0800

    Update deepseek_1_5b_ds.sh

--- examples/deepseek_1_5b_ds.sh
+++ examples/deepseek_1_5b_ds.sh
@@ -20,7 +20,7 @@ python3 -m verl.trainer.main \
     data.rollout_batch_size=128 \
     data.max_response_length=4096 \
     worker.actor.global_batch_size=32 \
-    trainer.experiment_name=deepseek_1.5b_deepscaler_1500_1122 \
+    trainer.experiment_name=deepseek_1.5b_deepscaler_1500_1124 \
     trainer.project_name=Length-LLM \
     trainer.n_gpus_per_node=8 \
     trainer.save_freq=25 \
@@ -33,4 +33,4 @@ python3 -m verl.trainer.main \
     algorithm.hit_cap=0.1 \
     algorithm.threshold=1500 \
     data.seed=20250521 \
-    worker.rollout.n=3
+    worker.rollout.n=5
