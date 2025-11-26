c001fcb627cf7f3723259663ab98aa3138ebf918
LC <changliu11@g.ucla.edu>
2025-11-26 01:31:41 -0800

    Update deepseek_1_5b_ds.sh

--- examples/deepseek_1_5b_ds.sh
+++ examples/deepseek_1_5b_ds.sh
@@ -6,7 +6,7 @@ export PYTHONUNBUFFERED=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 MODEL_PATH=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B  # replace it with your local file path
-CKPT_PATH=/data/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepseek_1.5b_deepscaler_1500_1124/global_step_200
+CKPT_PATH=/data/cliu/Length-Aware-LLM/checkpoints/Length-LLM/deepseek_1.5b_deepscaler_1500_1124/global_step_200/
 PROJECT_NAME=Length-LLM
 
 ray stop --force
